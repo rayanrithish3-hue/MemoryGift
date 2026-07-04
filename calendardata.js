f79e655024f4ad423aed5e47147051e3ac22dcca
rayanrithish3-hue <rayanrithish3@gmail.com>
2026-07-04 20:42:44 +0530

    Add month data structure with February, March, and April

--- calendardata.js
+++ calendardata.js
@@ -1,1 +1,33 @@
+const months=[
 
+{
+
+name:"February",
+
+days:28,
+
+memories:{}
+
+},
+
+{
+
+name:"March",
+
+days:31,
+
+memories:{}
+
+},
+
+{
+
+name:"April",
+
+days:30,
+
+memories:{}
+
+}
+
+];
