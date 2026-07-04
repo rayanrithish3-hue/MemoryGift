c36f9cc4db82fe93e594da7f2622db20e406e9fe
rayanrithish3-hue <rayanrithish3@gmail.com>
2026-07-04 20:44:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -73,3 +73,115 @@ memoryBtn.onclick=()=>{
 window.location.href="calendar.html";
 
 };
+
+if(window.location.pathname.includes("calendar")){
+
+let current=0;
+
+const container=document.getElementById("calendarContainer");
+
+const next=document.getElementById("nextBtn");
+
+const prev=document.getElementById("prevBtn");
+
+const popup=document.getElementById("popup");
+
+const popupDate=document.getElementById("popupDate");
+
+const popupMemory=document.getElementById("popupMemory");
+
+const close=document.getElementById("closePopup");
+
+function render(){
+
+container.innerHTML="";
+
+let month=months[current];
+
+let title=document.createElement("h2");
+
+title.innerHTML=month.name;
+
+container.appendChild(title);
+
+let grid=document.createElement("div");
+
+grid.className="calendar";
+
+for(let i=1;i<=month.days;i++){
+
+let box=document.createElement("div");
+
+box.className="day";
+
+box.innerHTML=i;
+
+if(month.memories[i]){
+
+box.classList.add("highlight");
+
+box.onclick=()=>{
+
+popup.style.display="flex";
+
+popupDate.innerHTML=i+" "+month.name;
+
+popupMemory.innerHTML=month.memories[i];
+
+};
+
+}
+
+grid.appendChild(box);
+
+}
+
+container.appendChild(grid);
+
+prev.style.display=current==0?"none":"inline-block";
+
+if(current==2){
+
+next.innerHTML="Click Me ❤️";
+
+}else{
+
+next.innerHTML="Next";
+
+}
+
+}
+
+render();
+
+next.onclick=()=>{
+
+if(current<2){
+
+current++;
+
+render();
+
+}else{
+
+window.location.href="hidden.html";
+
+}
+
+};
+
+prev.onclick=()=>{
+
+current--;
+
+render();
+
+};
+
+close.onclick=()=>{
+
+popup.style.display="none";
+
+};
+
+}
