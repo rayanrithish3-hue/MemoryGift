9a90e38d79467f14d657bd429676ba381c911a4c
rayanrithish3-hue <rayanrithish3@gmail.com>
2026-07-05 19:16:04 +0530

    Create calendarData.js

---
+++ calendarData.js
@@ -0,0 +1,52 @@
+const calendarData = {
+  "February": {
+    year: 2026,
+    month: 1,
+    memories: {
+      11: ["📍 Tiruppalai", "🍔 Yum Yards"],
+      18: ["🏍 Yamaha", "☕ Mug & Beans"],
+      19: ["🚶 TSM to Theppakulam"],
+      20: ["⛰ Thiruparangundram"],
+      21: ["🍽 Namma Kadai"],
+      22: ["🌄 Samanar Hills"],
+      26: ["🥣 Thirunagar (Mini Idly)"]
+    }
+  },
+
+  "March": {
+    year: 2026,
+    month: 2,
+    memories: {
+      2: ["☕ Mug & Beans"],
+      7: ["☕ Mug & Beans", "📍 Kalavasal"],
+      10: ["🌄 Samanar Hills", "🍽 Mini Tiffin"],
+      11: ["📍 Kalavasal", "🛍 Miniso", "🏛 Theppakulam"],
+      12: ["🧶 Crochet Gift"],
+      13: ["🏍 Bike Ride"],
+      17: ["☕ Mug & Beans"],
+      21: ["🛵 Scooty Water Wash", "🍔 Bell Jumbo", "🛍 Westside", "🏍 Bike Ride"],
+      23: ["🍦 Baskin Robbins", "🛍 Saravana Selvarathinam", "🍗 KFC"],
+      24: ["🍦 Baskin Robbins", "🛒 Food Bazaar"],
+      25: ["🍦 Baskin Robbins"],
+      29: ["📍 Kalavasal"]
+    }
+  },
+
+  "April": {
+    year: 2026,
+    month: 3,
+    memories: {
+      1: ["🪽 Siragugal", "💍 Lac Bangles", "🛍 Saravana Selvarathinam", "🏬 Vishaal De Mall", "🏛 Theppakulam"],
+      2: ["☕ Mug & Beans", "🥟 Pani Puri", "🍗 KFC"],
+      3: ["🍫 Chocolate Room"],
+      4: ["🧵 Tailor Shop", "🍛 Amman Hotel", "🌧 Villapuram Bridge Rain Drive"],
+      5: ["💇 Naturals", "🥤 Shell Bunk (Sweet Beer)", "🍫 Fudgy Milo"],
+      6: ["💍 Arunachalam Mahal Engagement", "☕ Food Bazaar (Mini Idly & Coffee)", "🏍 Moto Space"],
+      8: ["🍫 Belgian", "📸 Vishaal De Mall (Photo Booth)"],
+      12: ["🥤 Bao Bao (Fudgy Milo)", "🧸 Doll Shop"],
+      13: ["☕ Mug & Beans"],
+      17: ["🍫 Belgian", "🎁 Birthday Gifts"],
+      18: ["🛵 Scooty Ride", "🏍 Moto Space", "🛍 Trends", "🍫 Chocolate Search for Akka", "🍉 Watermelon Juice"]
+    }
+  }
+};
