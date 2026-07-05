da4682175be5c1d0581ce31a882d2cbc8c4f42bb
rayanrithish3-hue <rayanrithish3@gmail.com>
2026-07-05 19:26:42 +0530

    Add typing effect and button functionality in hidden.js

---
+++ hidden.js
@@ -0,0 +1,37 @@
+const message="You are someone who can't replace.";
+
+const typing=document.getElementById("typing");
+
+const sub=document.getElementById("subText");
+
+const btn=document.getElementById("continueBtn");
+
+let index=0;
+
+function type(){
+
+if(index<message.length){
+
+typing.innerHTML+=message.charAt(index);
+
+index++;
+
+setTimeout(type,70);
+
+}else{
+
+sub.innerHTML="Every memory in this little gift will always remain special. ❤️";
+
+btn.style.display="inline-block";
+
+}
+
+}
+
+type();
+
+btn.onclick=()=>{
+
+window.location.href="feedback.html";
+
+};
