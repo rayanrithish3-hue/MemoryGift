d30d110a426b8448c8ea53110a7142a79658c592
rayanrithish3-hue <rayanrithish3@gmail.com>
2026-07-22 01:11:35 +0530

    Update calendarData.js

--- calendarData.js
+++ calendarData.js
@@ -3,12 +3,12 @@ const calendarData = {
     year: 2026,
     month: 1,
     memories: {
-      11: ["📍 Tiruppalai", "🍔 Yum Yards", " Friend's Mehandi Function"],
-      18: ["🏍 Scooty Service", "☕ Mug & Beans"],
-      19: ["🚶 TSM to Theppakulam first long scooty ride for you"],
-      20: ["⛰ Thiruparangundram appa amma kooda barbeque nation"],
-      21: ["🍽 Namma Kadai la night dinner"],
-      22: ["🌄 Enna First time Samanar Hills kottitu pona"],
+      11: ["📍Tiruppalai la un Frnd Harinee oda Mehandi Function ku unna drop pannen aprm Meenakshi amman temple la irunthu unna pickup pannitu nee church paathadhu illa nu sonna unna church kootitu ponen first time nee church vantha aprm Yum Yards la chicken loaded fries saaptutu clg poitom"],
+      18: ["🏍 Scooty Service eduthutu aprm Mug & Beans poitu clg vanthu scooty ya parking la vitutu clg velila iruntha waffles kadaila waffles saaptom"],
+      19: ["🚶Clg la irunthu Theppakulam vara first time traffic la scooty a drive panna pudhu scooty vaangi first time long journey for you enakum unkooda scooty la vanthathu adhaan first time aprm unaku pudicha 90's mittai theppakulam la vaangunom"],
+      20: ["⛰ Appa Amma Akka kooda barbeque nation porathuku unna thiruparangundram la drop pannitu vanthen"],
+      21: ["🍽 Namma Kadai la night dinner saaptom"],
+      22: ["🌄 Enna First time Samanar Hills kottitu pona anga poitu photo eduthutu aprm gowri sankar hotel la saaptutu trends poitu unaku dress vaangunom"],
       26: ["🥣 Thirunagar la Mini Idly cravings a fulfill pannom"]
     }
   },
@@ -18,14 +18,14 @@ const calendarData = {
     month: 2,
     memories: {
       2: ["☕ Mug & Beans"],
-      7: ["☕ Mug & Beans", "📍 Kalavasal la pani poori saaptom aprm light balloon, bommai vaangunom"],
+      7: ["☕ Mug & Beans poitu aprm Kalavasal la pani poori saaptom aprm light balloon, bommai laam vaangunom"],
       10: ["🌄 Mini Tiffin order potu Samanar Hills la poi saaptom"],
-      11: ["📍 Kalavasal", "🛍 Miniso", "🏛 Theppakulam", "inga laam ponom"],
+      11: ["📍 Kalavasal, Miniso, Theppakulam inga laam ponom"],
       12: ["🧶 Crochet Gift for your Birthday"],
       13: ["🏍 First time ennoda Bike drive panna"],
       17: ["☕ Mug & Beans ponom"],
-      21: ["🛵 Scooty Water Wash vitutu aprm 🍔 Bell Jumbo ponom 🛍 Westside ponom Church ponom aprm yen Bike drive panna"],
-      23: ["SBM aprm ella exams layum pass aaita nu 🍦 Baskin Robbins la icecream saaptutu 🛍 Saravana Selvarathinam ponom aprm🍗 KFC ponom"],
+      21: ["🛵 Scooty Water Wash vitutu aprm Bell Jumbo ponom 🛍 Westside ponom Church ponom aprm yen Bike drive panna"],
+      23: ["SBM aprm ella exams layum pass aaita nu Baskin Robbins la icecream saaptutu Saravana Selvarathinam ponom aprm KFC ponom"],
       24: ["🍦 Baskin Robbins 2nd time icecream saapda ponom aprm Food Bazaar ponom"],
       25: ["🍦 Baskin Robbins 3rd time icecream saapda ponom poitu lifestyle la sayonara ku dress eduthom unaku"],
       29: ["📍 Nee Happy Raj movie ku poitu vanthu headache a heal panna food bazaar poi coffee kudichitu bike la varumbodhu song ketutu vanthom"]
@@ -36,17 +36,17 @@ const calendarData = {
     year: 2026,
     month: 3,
     memories: {
-      1: ["🪽 Siragugal poitu saaptutu un Bday ku gift aa naa hyderabad poi vangitu vantha Lac Bangles kuduthen 🛍 Saravana Selvarathinam aprm Vishaal De Mall aprm Theppakulam inga laam poitu jolly aa irunthuchi"],
-      2: ["☕ Mug & Beans", "🥟 Pani Puri", "🍗 KFC"],
-      3: ["🍫 Chocolate Room"],
-      4: ["🧵 Tailor Shop", "🍛 Amman Hotel", "🌧 Villapuram Bridge Rain Drive"],
-      5: ["💇 Naturals", "🥤 Shell Bunk (Sweet Beer)", "🍫 Fudgy Milo"],
-      6: ["💍 Arunachalam Mahal Engagement", "☕ Food Bazaar (Mini Idly & Coffee)", "🏍 Moto Space"],
-      8: ["🍫 Belgian", "📸 Vishaal De Mall (Photo Booth)"],
-      12: ["🥤 Bao Bao (Fudgy Milo)", "🧸 Doll Shop"],
-      13: ["☕ Mug & Beans"],
-      17: ["🍫 Belgian", "🎁 Birthday Gifts"],
-      18: ["🛵 Scooty Ride", "🏍 Moto Space", "🛍 Trends", "🍫 Chocolate Search for Akka", "🍉 Watermelon Juice"]
+      1: ["🪽 Siragugal poitu saaptutu un Bday ku gift aa naa hyderabad poi vangitu vantha Lac Bangles kuduthen 🛍 Saravana Selvarathinam aprm Vishaal De Mall aprm Theppakulam ponom anga irunthu un fav dosa saapda famous dosa kadaiku kootitu ponen"],
+      2: ["☕ Mug & Beans poitu anga irunthu kalavasal poitu KFC la order potutu Pani Poori saapda poitom"],
+      3: ["🍫 Chocolate Room nalla irukum nu enna kootitu pona enaku pudichi irunthuchi but unaku pudikala unnoda expectations a meet pannala"],
+      4: ["🧵 Friend marriage ku alter kudutha saree a Tailor Shop poitu vaangitu vanthom aprm un fav mini idly saapda Amman Hotel ponom but kedaikala aprm Villapuram Bridge la first time Rain 🌧 la Drive panna traffic la"],
+      5: ["💇 Friend oda engagement ku blow dry panna Naturals ponom poitu Shell petrol Bunk la Sweet Beer vaangitu aprm un fav 🍫 Fudgy Milo parcel vaangitu ponom"],
+      6: ["💍 Un Friend engagement ku Arunachalam Mahal ponom poitu Food Bazaar la Mini Idly parcel vaangitu Coffee kudichitu aprm nee mahal la chocolate keta enkita apo illa adhaan 🏍 Moto Space ku poitu unaku chocolate vaangi kuduthen"],
+      8: ["🍫 Belgian cafe ponom aprm February 11th apo enkooda velila varapo pota same costume potrunthom so Vishaal De Mall ku poitu Photo Booth la super super photos eduthom"],
+      12: ["🥤 Bao Bao ku poitu un fav Fudgy Milo kudichom aprm snap la oru recreate pannom because march 24th apo velila porapo potruntha same costume thaan april 12th um potrunthom aprm 🧸 Doll Shop ponom"],
+      13: ["☕ Mug & Beans ku first time um unkooda thaan vanthen and last time um unkooda thaan vanthen aprm first time heart pota cappucinno kudichom"],
+      17: ["🍫 Belgian cafe ponom enaku nee customized packing la enaku pudicha perfumes, coffee paper note aprm ennoda fav chocolate laam Birthday Gifts a kudutha idha marakavey maaten"],
+      18: ["🛵 My Birthday and Last day of clg un Scooty la last aa oru Ride ponom aprm Moto Space la juice kudichitu Trends poitu unaku scarf & dress vaangitu unnoda akka ku kuduka akka keta chocolate a thedunom but kedaikala aprm unaku pudicha Watermelon Juice vaangi kuduthutu naa munnar kelambiten"]
     }
   }
 };
