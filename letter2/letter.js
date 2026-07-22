93c0256dff87a30b5bc263f1b47b224aea1fddfc
rayanrithish3-hue <rayanrithish3@gmail.com>
2026-07-22 17:43:12 +0530

    Update letter.js

--- letter2/letter.js
+++ letter2/letter.js
@@ -1,1 +1,18 @@
+const openBtn = document.getElementById("openBtn");
+const letter = document.getElementById("letter");
 
+let opened = false;
+
+openBtn.addEventListener("click", () => {
+
+    if (opened) return;
+
+    opened = true;
+
+    letter.classList.add("show");
+
+    openBtn.textContent = "Opened";
+
+    openBtn.disabled = true;
+
+});
